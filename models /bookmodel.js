13482c5bfa84d9f1f5d37ce7ff3d4f4a305fe309
Hossprog <hossprog@gmail.com>
2020-06-14 12:56:06 +0200

    git modelsol

--- models /bookmodel.js
+++ models /bookmodel.js
@@ -1,50 +1,45 @@
 const mongoose = require('mongoose')
-//const path = require('path')
 
 const bookSchema = new mongoose.Schema({
-        
-    title : {
-        type : String,
-        required : true 
-    },
-    description : {
-        type : String,
-    },
-    publishDate : {
-        type : Date,//famma type date yaani remember it 
-        required : true 
-    },
-    pageCount : {
-        type : Number,
-        required : true
-    },
-    createdAt : {
-        type : Date, 
-        required : true,
-        default : Date.now //this will insert the actual date by default 
-    },
-    CoverImage : {
-        type : Buffer,
-        required : true 
-    },
-    CoverImageType : {
-        type: String,
-        required: true 
-    },
-    author : {
-        type : mongoose.Schema.Types.ObjectId,
-        required : true ,
-        ref : 'Author'//this clname has to match the name set for the model
-        }
-    })
-
-bookSchema.virtual('coverImagePath').get(function() {//indicating the file that we wanna query
-    if(this.CoverImage && this.CoverImageType){
-        return `data:${this.coverImageType};charset=utf-8;base64,${this.CoverImage.toString('base64')}`
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  publishDate: {
+    type: Date,
+    required: true
+  },
+  pageCount: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  coverImage: {
+    type: Buffer,
+    required: true
+  },
+  coverImageType: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Author'
+  }
 })
 
+bookSchema.virtual('coverImagePath').get(function() {
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+  }
+})
 
-// Author is the name of the model(database table )with the authorschema as a schema
-// the same cycle here need to export it and then require it to the routing page in which u need it 
-module.exports = mongoose.model('Book',bookSchema)
+module.exports = mongoose.model('Book', bookSchema)
